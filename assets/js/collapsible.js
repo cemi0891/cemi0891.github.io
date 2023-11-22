19e5380fbd37e5b9372ba005b91845335ee13880
Raul Dominguez <raul.dominguez@codster.io>
2023-11-22 16:30:26 -0600

    feature(menu) collapsible

--- assets/js/collapsible.js
+++ assets/js/collapsible.js
@@ -1,14 +1,27 @@
-var coll = document.getElementsByClassName("collapsible");
-var i;
+$(document).ready(function(){
+  $(".menu-item, .sub-menu-item").click(function(event){
+  event.stopPropagation(); // Prevent the click event from propagating to the parent menu item
 
-for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var content = this.nextElementSibling;
-    if (content.style.display === "block") {
-      content.style.display = "none";
-    } else {
-      content.style.display = "block";
-    }
+  var subMenu = $(this).next(".sub-menu");
+  
+  if (subMenu.length > 0) {
+      $(this).toggleClass("active");
+      subMenu.slideToggle();
+  }
   });
-}
+});
+
+// var coll = document.getElementsByClassName("collapsible");
+// var i;
+
+// for (i = 0; i < coll.length; i++) {
+//   coll[i].addEventListener("click", function() {
+//     this.classList.toggle("active");
+//     var content = this.nextElementSibling;
+//     if (content.style.display === "block") {
+//       content.style.display = "none";
+//     } else {
+//       content.style.display = "block";
+//     }
+//   });
+// }
