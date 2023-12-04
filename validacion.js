9e7433a638d715e7b3d80a1cc0fbc376c4e3b80e
cristian maraver <maraver.cristiane@gmail.com>
2023-12-04 10:02:38 -0600

    cambio

---
+++ validacion.js
@@ -0,0 +1,14 @@
+function validar (){
+
+    var c="usuario";
+    var u="2023"; 
+    var user=document.getElementById('login').value
+    var pass=document.getElementById('password').value
+
+    console.log(user);
+    if(user==c && pass==u){           
+    window.location="home.html";}
+    else{
+    alert("Ingresa usuario y contraseña correcta");
+    }
+}
